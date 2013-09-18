bc6317853140ce97083934ff5bc20dededd90cc9
Modesto <augusto.modesto@live.com>
2013-09-18 00:20:53 -0300

    Refinamento classe Aluno
    Adicionados comentários e refatorado classe.

--- Classe/Aluno.java
+++ Classe/Aluno.java
@@ -1,6 +1,11 @@
+
+//------------------------------ CLASSE ALUNO ------------------------------------
 package Classes;
 
 public class Aluno {
+
+//------------------------------ ATRIBUTOS ------------------------------------
+    
 
 	//Atributos da classe Aluno
 	private int matricula;
@@ -12,103 +17,110 @@ public class Aluno {
 	private Responsavel responsavel;
 	private Disciplina disciplinas[] = {};
 	
-	//Gets e Sets das classes
-	public int getMatricula() {
-		return matricula;
-	}
+//------------------------------ GET'S ------------------------------------
+        
+    //Gets dos Atributos
+    public int getMatricula() {
+        return matricula;
+    }
 
-	public void setMatricula(int matricula) {
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public Responsavel getResponsavel() {
+        return responsavel;
+    }
+
+    public Disciplina[] getDisciplinas() {
+        return disciplinas;
+    }
+    	
+//------------------------------ SET'S ------------------------------------
+        
+    //Sets dos Atributos
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public void setResponsavel(Responsavel responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public void setDisciplinas(Disciplina[] disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+  
+//------------------------------ METODOS E CONSTRUTORES ------------------------------------
+       
+        
+        //Construtor sobrecarregado para n�o haver instancias sem alguns dados importantes 
+	public Aluno(int matricula, String nome, String endereco) {
 		this.matricula = matricula;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
 		this.nome = nome;
+		this.endereco = endereco; 	
 	}
-
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSerie() {
-		return serie;
-	}
-
-	public void setSerie(String serie) {
-		this.serie = serie;
-	}
-	
-	public Responsavel getResponsavel() {
-		return responsavel;
-	}
-
-	public void setResponsavel(Responsavel responsavel) {
-		this.responsavel = responsavel;
-	}
-
-	public Disciplina[] getDisciplinas() {
-		return disciplinas;
-	}
-
-	public void setDisciplinas(Disciplina[] disciplinas) {
-		this.disciplinas = disciplinas;
-	}
-
+        
         //M�todo para conter os n�meros adicionados no vetor
 	public void adicionarDisciplina(Disciplina disciplina) {
-		int tamanhoAnterior = this.disciplinas.length;
+		int tamanhoAnterior = this.getDisciplinas().length;
 		if(tamanhoAnterior-1 >= 10) {
 			System.out.println("N�o � permitido adicionar mais do que 10 disciplinas.");
 		} else {
 			Disciplina[] novasDisciplinas = new Disciplina [tamanhoAnterior];
 			for(int j=0; j < tamanhoAnterior; j++) {
-				novasDisciplinas[j] = this.disciplinas[j];
+				novasDisciplinas[j] = this.getDisciplinas()[j];
 			}
 			novasDisciplinas[novasDisciplinas.length-1] = disciplina;
 			this.setDisciplinas(novasDisciplinas);
 		}
 	}
         
-	//Construtor sobrecarregado para n�o haver instancias sem alguns dados importantes 
-	public Aluno(int matricula, String nome, String endereco) {
-		this.matricula = matricula;
-		this.nome = nome;
-		this.endereco = endereco; 	
-	}
-	
 	//Metodo no qual ser� poss�vel matricular novos alunos
 	public void matricularAluno(int matricula, String nome, String dataNascimento,
 			String endereco, String email, String serie ) {
-		this.matricula = matricula;
-		this.nome= nome;
-		this.dataNascimento = dataNascimento;
-		this.endereco = endereco;
-		this.email = email;
-		this.serie = serie;
+		this.setMatricula(matricula);
+		this.setNome(nome);
+		this.setDataNascimento(dataNascimento);
+		this.setEndereco(endereco);
+		this.setEmail(email);
+		this.setSerie(serie);
 		System.out.println("Dados cadastrados!");
 	}
-
 }
