0d54f5eb1356d496c93637d5ad06cbc42fa7f82e
Modesto <augusto.modesto@live.com>
2013-09-18 00:33:55 -0300

    Refinamento classe Professor
    Adicionados comentarios e refatorado a classe.

--- Classe/Professor.java
+++ Classe/Professor.java
@@ -1,7 +1,11 @@
+
+//------------------------------ CLASSE PROFESSOR ------------------------------------
 package Classes;
 
 public class Professor {
 
+//------------------------------ ATRIBUTOS ------------------------------------
+    
 	//Atributos da classe Professor
 	private String nome;
 	private int codigoIdentificacao;
@@ -12,95 +16,103 @@ public class Professor {
 	private Disciplina disciplinas[] = {};
 	private Sala sala;
 	
-	//Gets e Sets das classes
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public int getCodigoIdentificacao() {
-		return codigoIdentificacao;
-	}
-
-	public void setCodigoIdentificacao(int codigoIdentificacao) {
-		this.codigoIdentificacao = codigoIdentificacao;
-	}
-
-	public String getGraduacao() {
-		return graduacao;
-	}
-
-	public void setGraduacao(String graduacao) {
-		this.graduacao = graduacao;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public Disciplina[] getDisciplinas() {
-		return disciplinas;
-	}
-
-	public void setDisciplinas(Disciplina[] disciplinas) {
-		this.disciplinas = disciplinas;
-	}
-
-	public Sala getSala() {
-		return sala;
-	}
-
-	public void setSala(Sala sala) {
-		this.sala = sala;
-	}
+//------------------------------ GET'S ------------------------------------
         
+    //Gets e Sets das classes
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigoIdentificacao() {
+        return codigoIdentificacao;
+    }
+
+    public String getGraduacao() {
+        return graduacao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Disciplina[] getDisciplinas() {
+        return disciplinas;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+    
+//------------------------------ SET'S ------------------------------------
+        
+    //Sets dos Atributos
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCodigoIdentificacao(int codigoIdentificacao) {
+        this.codigoIdentificacao = codigoIdentificacao;
+    }
+
+    public void setGraduacao(String graduacao) {
+        this.graduacao = graduacao;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDisciplinas(Disciplina[] disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+          
+//------------------------------ METODOS E CONSTRUTORES ------------------------------------
+        
+        
+        //Construtor sem sobrecarga (vazio)
+	public Professor() {
+            
+	}
+
         //M�todo para conter os n�meros adicionados no vetor
 	public void adicionarDisciplina(Disciplina disciplina) {
-		int tamanhoAnterior = this.disciplinas.length;
+		int tamanhoAnterior = this.getDisciplinas().length;
 		if(tamanhoAnterior-1 >= 10) {
 			System.out.println("N�o � permitido adicionar mais do que 10 disciplinas.");
 		} else {
 			Disciplina[] novasDisciplinas = new Disciplina [tamanhoAnterior];
 			for(int j=0; j < tamanhoAnterior; j++) {
-				novasDisciplinas[j] = this.disciplinas[j];
+				novasDisciplinas[j] = this.getDisciplinas()[j];
 			}
 			novasDisciplinas[novasDisciplinas.length-1] = disciplina;
 			this.setDisciplinas(novasDisciplinas);
 		}
 	}
 
-	//Construtor sem sobrecarga (vazio)
-	public Professor() {
-		
-	}
-
 	//Metodo no qual � possivel modificar a gradua��o informado pelo professor
 	public void atualizarGraduacao (String graduacao) {
-		this.graduacao = graduacao;
+		this.setGraduacao(graduacao);
 		
-		System.out.println("O nome e-mail cadastrado �: " + this.graduacao);
+		System.out.println("O nome e-mail cadastrado �: " + this.getGraduacao());
 	}
 }
