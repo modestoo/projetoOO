c77ab480d704b5b26dc1c64c5c28cfac44f76521
tatielen <tatielen.rodrigues@gmail.com>
2013-09-18 15:41:27 -0300

    Adicionado primeiro teste de relacionamento
    Adicionando o primeiro teste de relacionamento.

---
+++ Testes/RelacionamentoITest.java
@@ -0,0 +1,20 @@
+package Testes;
+
+//Importacao das classes para utilizacao
+import Classes.Responsavel;
+import Classes.Aluno;
+
+//Teste do primeiro relacionamento. Classes: Responsavel - Aluno
+public class RelacionamentoITest {
+
+	//Construtor da classe com seus parametros vazios
+	public RelacionamentoITest() {
+		
+	}
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
