494a185b61d4107ae00286b4056c131b34a8eabb
Modesto <augusto.modesto@live.com>
2013-09-23 16:51:23 -0300

    Criada classe de Teste
    Criada classe para teste de relacionamento (RelacionamentoIITest).

---
+++ Testes/RelacionamentoIITest.java
@@ -0,0 +1,17 @@
+package Testes;
+
+//Importacao das classes para utilizacao
+import Classes.Direcao;
+import Classes.Secretaria;
+
+//Teste do segundo relacionamento. Classes: Direcao - Secretaria
+public class RelacionamentoIITest {
+
+    //Construtor da classe com seus parametros vazios
+    public RelacionamentoIITest() {
+    }
+        
+    public static void main(String[] args) {
+        
+    }
+}
