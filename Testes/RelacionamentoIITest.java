870da49931ef6956d3c1a677956699d693774484
Modesto <augusto.modesto@live.com>
2013-09-23 17:44:04 -0300

    Adicionado Teste
    Adicionado os testes dos relacionamentos

--- Testes/RelacionamentoIITest.java
+++ Testes/RelacionamentoIITest.java
@@ -12,6 +12,19 @@ public class RelacionamentoIITest {
     }
         
     public static void main(String[] args) {
+        Direcao direcao1 = new Direcao();
+        direcao1.setDiretor("Diretor1");
+        direcao1.setCoordenador("Coordenador1");
+        direcao1.setPsicopedagogo("Psicopedagogo1");
+        direcao1.setOrientador("Orientador1");
+        
+        Secretaria secretaria1 = new Secretaria();
+        secretaria1.setMatricula("123456");
+        secretaria1.setCpf("123.456.789-12");
+        secretaria1.setNome("Secretaria1");
+        secretaria1.setFuncao("Assistente1");
+        
+        direcao1.criarSecretaria(secretaria1);
         
     }
 }
