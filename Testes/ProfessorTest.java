905091583a9e6396ed2fec1fe95a00608e9f8307
Modesto <augusto.modesto@live.com>
2013-10-06 20:37:35 -0300

    Reformualdo teste JUnit (Professor)
    Adicionado novos testes de unidade a classe

--- Testes/ProfessorTest.java
+++ Testes/ProfessorTest.java
@@ -1,11 +1,12 @@
 package Testes;
 
-import Classes.Professor;
 import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import Classes.Professor;
 
 public class ProfessorTest {
 
@@ -18,7 +19,47 @@ public class ProfessorTest {
 	}
 
 	@Test
-	public void testAtualizarGraduacao() {
-		}
-}
+	public void testSetEspecializacao() {
+		Professor p1 = new Professor("p1", 123, "prof", "9876 - 6789");
+		p1.setEspecializacao("Pedagocia");
+		assertEquals(p1.getEspecializacao(), "Pedagocia");
+		
+	}
 
+	@Test
+	public void testSetHorarioDeAula() {
+		Professor p1 = new Professor("p1", 123, "prof", "9876 - 6789");
+		p1.setHorarioDeAula("7:00 as 12:00");
+		assertEquals(p1.getHorarioDeAula(), "7:00 as 12:00");
+		
+	}
+
+	@Test
+	public void testGetNome() {
+		Professor p1 = new Professor("p1", 123, "prof", "9876 - 6789");
+		assertEquals(p1.getNome(), "p1");
+		
+	}
+
+	@Test
+	public void testGetCodigoIdentificacao() {
+		Professor p1 = new Professor("p1", 123, "prof", "9876 - 6789");
+		assertEquals(p1.getCodigoIdentificacao(), 123);
+		
+	}
+
+	@Test
+	public void testGetFuncao() {
+		Professor p1 = new Professor("p1", 123, "prof", "9876 - 6789");
+		assertEquals(p1.getFuncao(), "prof");
+		
+	}
+
+	@Test
+	public void testGetTelefone() {
+		Professor p1 = new Professor("p1", 123, "prof", "9876 - 6789");
+		assertEquals(p1.getTelefone(), "9876 - 6789");
+		
+	}
+
+}
