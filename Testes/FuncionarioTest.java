74901a52c61753019deb8128bb67e866efc9578a
Modesto <augusto.modesto@live.com>
2013-10-13 18:31:39 -0300

    Reformulado teste de unidade (Funcionario)

--- Testes/FuncionarioTest.java
+++ Testes/FuncionarioTest.java
@@ -20,13 +20,80 @@ public class FuncionarioTest {
 		System.out.println("Iniciando...");
 	}
 
+	
 	@Test
-	public void testFuncionario() {
-		Funcionario funcionario1 = new Funcionario("Nome1", 12345, "Funcao1", "33333333");
-		assertEquals("Nome1", funcionario1.getNome());
-		assertEquals(12345, funcionario1.getCodigoIdentificacao());
-		assertEquals("Funcao1", funcionario1.getFuncao());
-		assertEquals("33333333", funcionario1.getTelefone());
+	public void testGetNome() {
+		Funcionario f1 = new Funcionario("f1",123, "func", "123");
+		assertEquals(f1.getNome(), "f1");
+		
+	}
+	
+	@Test
+	public void testGetCpf() {
+		Funcionario f1 = new Funcionario("f1",123, "func", "123");
+		f1.setCpf("123");
+		assertEquals(f1.getCpf(), "123");
+		
+	}
+	
+	@Test
+	public void testGetRg() {
+		Funcionario f1 = new Funcionario("f1",123, "func", "123");
+		f1.setRg(123);
+		assertEquals(f1.getRg(), 123);
+		
+	}
+
+	@Test
+	public void testGetCodigoIdentificacao() {
+		Funcionario f1 = new Funcionario("f1",123, "func", "123");
+		assertEquals(f1.getCodigoIdentificacao(), 123);
+		
+	}
+
+	@Test
+	public void testGetFuncao() {
+		Funcionario f1 = new Funcionario("f1",123, "func", "123");
+		assertEquals(f1.getFuncao(), "func");
+		
+	}
+
+	@Test
+	public void testGetTelefone() {
+		Funcionario f1 = new Funcionario("f1",123, "func", "123");
+		assertEquals(f1.getTelefone(), "123");
+		
+	}
+
+	@Test
+	public void testGetEndereco() {
+		Funcionario f1 = new Funcionario("f1",123, "func", "123");
+		f1.setEndereco("Super 11");
+		assertEquals(f1.getEndereco(), "Super 11");
+		
+	}
+	
+	@Test
+	public void testGetEmail() {
+		Funcionario f1 = new Funcionario("f1",123, "func", "123");
+		f1.setEmail("gmail.com");
+		assertEquals(f1.getEmail(), "gmail.com");
+		
+	}
+	
+	@Test
+	public void testGetPresenca() {
+		Funcionario f1 = new Funcionario("f1",123, "func", "123");
+		f1.setPresenca(true);
+		assertTrue(true);
+		
+	}
+	
+	@Test
+	public void testGetDescricaoRelatorio() {
+		Funcionario f1 = new Funcionario("f1",123, "func", "123");
+		f1.setDescricaoRelatorio("bla bla");
+		assertEquals(f1.getDescricaoRelatorio(), "bla bla");
 		
 	}
 
