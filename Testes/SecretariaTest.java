6494f5d134acb05cc953e0aea6ad849b43207a75
Modesto <augusto.modesto@live.com>
2013-10-13 19:22:26 -0300

    Reformulado teste de unidade (Secretaria)

--- Testes/SecretariaTest.java
+++ Testes/SecretariaTest.java
@@ -10,8 +10,13 @@ import Classes.Secretaria;
 
 public class SecretariaTest {
 
+	Secretaria s1 = null;
+	
 	@Before
 	public void setUp() throws Exception {
+		s1 = new Secretaria("s1", 123, "sec", "1234 - 6543");
+		s1.setFolga("terca feira");
+	
 	}
 
 	@After
@@ -20,17 +25,16 @@ public class SecretariaTest {
 	
 	@Test
 	public void testGetFolga() {
-		Secretaria s1 = new Secretaria("s1", 123, "sec", "1234 - 6543");
-		s1.setFolga("terca feira");
-		assertEquals(s1.getFolga(), "terca feira");
+		assertEquals("terca feira", s1.getFolga());
+		
 	}
 	
 	@Test
 	public void testGetAgenda() {
-		Secretaria s1 = new Secretaria("s1", 123, "sec", "1234 - 6543");
 		String[] Agendas = {"Segunda","Terca"};
 		s1.setAgendas(Agendas);
-		assertArrayEquals(s1.getAgendas(), Agendas);
+		assertArrayEquals(Agendas, s1.getAgendas());
+		
 	}
 
 }
