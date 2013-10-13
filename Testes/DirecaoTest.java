9483a20ec4d1dacb33aacff64313713a80f3f493
Modesto <augusto.modesto@live.com>
2013-10-13 19:18:10 -0300

    Reformulado teste de unidade (Direcao)

--- Testes/DirecaoTest.java
+++ Testes/DirecaoTest.java
@@ -5,11 +5,20 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import Classes.Direcao;
 
 public class DirecaoTest {
 
+	Direcao d1 = null;
+	
 	@Before
 	public void setUp() throws Exception {
+		d1 = new Direcao();
+		d1.setDiretor("Francisco");
+		d1.setCoordenador("Joao");
+		d1.setOrientador("Marcos");
+		d1.setPsicopedagogo("Maria");
+		
 	}
 
 	@After
@@ -17,12 +26,31 @@ public class DirecaoTest {
 	}
 
 	@Test
-	public void testAdvertirAluno() {
+	public void testGetDiretor() {
+		assertEquals("Francisco", d1.getDiretor());
+	}
 	
+	@Test 
+	public void testGetCoordenador() {
+		assertEquals("Joao", d1.getCoordenador());
+		
+	}
+	
+	@Test 
+	public void testGetOrientador() {
+		assertEquals("Marcos", d1.getOrientador());
+		
+	}
+	
+	@Test 
+	public void testGetPsicopedagogo() {
+		assertEquals("Maria", d1.getPsicopedagogo());
+		
 	}
 	
 	@Test 
 	public void testCriarSecretaria() {
+		assertEquals("Joao", d1.getCoordenador());
 		
 	}
 
