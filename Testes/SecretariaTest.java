16b6b655c28f27bfd2020dc5defdb23df116c0fd
Modesto <augusto.modesto@live.com>
2013-10-13 18:33:42 -0300

    Reformulado teste de unidade (Secretaria)

--- Testes/SecretariaTest.java
+++ Testes/SecretariaTest.java
@@ -17,40 +17,20 @@ public class SecretariaTest {
 	@After
 	public void tearDown() throws Exception {
 	}
-
-	@Test
-	public void testGetNome() {
-		Secretaria s1 = new Secretaria("s1", 123, "sec", "1234 - 6543");
-		assertEquals(s1.getNome(), "s1");
-		
-	}
-		
-	@Test
-	public void testGetTelefone() {
-		Secretaria s1 = new Secretaria("s1", 123, "sec", "1234 - 6543");
-		assertEquals(s1.getTelefone(), "1234 - 6543");
-			
-	}
-	
-	@Test
-	public void testGetFuncao() {
-		Secretaria s1 = new Secretaria("s1", 123, "sec", "1234 - 6543");
-		assertEquals(s1.getFuncao(), "sec");
-			
-	}
-
-	@Test
-	public void testGetcodigoIdentificacao() {
-		Secretaria s1 = new Secretaria("s1", 123, "sec", "1234 - 6543");
-		assertEquals(s1.getCodigoIdentificacao(), 123);
-			
-	}
 	
 	@Test
 	public void testGetFolga() {
 		Secretaria s1 = new Secretaria("s1", 123, "sec", "1234 - 6543");
 		s1.setFolga("terca feira");
 		assertEquals(s1.getFolga(), "terca feira");
+	}
+	
+	@Test
+	public void testGetAgenda() {
+		Secretaria s1 = new Secretaria("s1", 123, "sec", "1234 - 6543");
+		String[] Agendas = {"Segunda","Terca"};
+		s1.setAgendas(Agendas);
+		assertArrayEquals(s1.getAgendas(), Agendas);
 	}
 
 }
