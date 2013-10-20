23c276f4e144d4ef76432b548215cf007d284a86
jamilydesouza <jamily_ms2@hotmail.com>
2013-10-20 20:29:51 -0200

    PessoaTest

--- Classes/Pessoa.java
+++ Classes/Pessoa.java
@@ -11,10 +11,60 @@ public abstract class Pessoa {
 	protected String endereco;
 	protected String email;
 	
+	//Construtor vazio
 	public Pessoa() {
 		// TODO Auto-generated constructor stub
 	}
-	
+//------------------------------ GET'S ------------------------------------	
+	//Gets dos atributos
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public int getRg() {
+		return rg;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+//------------------------------ SET'S ------------------------------------	    
+	// Sets dos atributos
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public void setRg(int rg) {
+		this.rg = rg;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 
 }
