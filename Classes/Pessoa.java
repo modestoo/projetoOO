a4ca56627ee9dbee6cb41d591a694467c63e0ced
jamilydesouza <jamily_ms2@hotmail.com>
2013-10-20 19:58:30 -0200

    Classe abstrata
    Atributos da classe

--- Classes/Pessoa.java
+++ Classes/Pessoa.java
@@ -1,9 +1,20 @@
 package Classes;
 
 public abstract class Pessoa {
-
+//------------------------------ ATRIBUTOS ------------------------------------
+    
+	//atributos da classe pessoa
+	protected String nome;
+	protected String cpf;
+	protected int rg;
+	protected String telefone;
+	protected String endereco;
+	protected String email;
+	
 	public Pessoa() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	
 
 }
