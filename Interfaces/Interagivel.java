86f92f3341eaa0ffbc2b2151cc99dd766dc330dd
Modesto <augusto.modesto@live.com>
2013-10-26 21:24:47 -0200

    Adicionados paremetros aos metodos da Interface Interagivel

--- Interfaces/Interagivel.java
+++ Interfaces/Interagivel.java
@@ -3,6 +3,6 @@ package Interfaces;
 public interface Interagivel {
 	
 	//Metodo que permitar objetos enviar mensagens entre si
-	public void startaTexto();
+	public void startaTexto(String texto, String[] contatos);
 
 }
