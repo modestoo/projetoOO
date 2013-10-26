b397a35ff75ab2390426c6b889a51eaa8b2afe83
Modesto <augusto.modesto@live.com>
2013-10-26 20:32:19 -0200

    Criado a interface Funcionavel

---
+++ Interfaces/Funcionavel.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public interface Funcionavel {
+	
+	//Metodo que permite gerar um relatorio
+	public void gerarRelatorio();
+	
+	//Metodo que permite um funcionario bater seu ponto
+	public void baterPonto();
+
+}
